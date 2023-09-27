68c429124b2c716223e87bed7b6cb57a8b64dc67
Bryan Gabriel-Atangana Mboa <bgabrielata@ensc.fr>
2023-09-27 10:17:04 +0200

    Initialisation de la classe tableau

---
+++ Tableau.cs
@@ -0,0 +1,10 @@
+// Affichage tableau 
+
+public class Tableau{
+    public int x {get; set;}
+    public int y {get; set;}
+    public char[,] tab = new char[x,y];
+    public Tableau (){
+
+    }
+}
