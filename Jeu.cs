de18cce67d2f985509f24a8cc2d35f13cc985a0c
Joanne Gangapal <joannegangapal@ensc.fr>
2023-09-27 10:25:17 +0200

    début Jeu

---
+++ Jeu.cs
@@ -0,0 +1,18 @@
+//Cette classe permet de lancer le jeu 
+
+public class Jeu
+{
+    private Tableau plateau;
+    private int tour;
+    public Jeu()
+    {
+        plateau = new Tableau();
+    }
+
+    public bool Gagner()
+    {
+        return false;
+    }
+
+
+}
